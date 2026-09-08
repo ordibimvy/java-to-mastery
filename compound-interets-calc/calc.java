7c7cde1c518c19bc060553f32bfb423e0beb0efb
ordibimvy <ordibimvy10@gmail.com>
2026-09-08 12:03:28 -0400

    Create calc.java
    A simple coumpound interets calculator with its known limitations

---
+++ compound-interets-calc/calc.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[] args) {
+
+        //Compound Interest Calculator
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int times_compounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the # of times coumpounded per year: ");
+        times_compounded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / times_compounded, times_compounded * years);
+
+        System.out.printf("The amount after %d years is $%.2f", years, amount);
+
+        scanner.close();
+    }       
+}
